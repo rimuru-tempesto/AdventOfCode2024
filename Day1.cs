a23cae5fd763e2778b3943d89ff69054694c85c6
rimuru-tempesto <36051904+rimuru-tempesto@users.noreply.github.com>
2024-12-02 09:11:23 +0200

    Add files via upload

---
+++ Day1.cs
@@ -0,0 +1,36 @@
+﻿PartOne();
+PartTwo();
+
+static void PartOne() {
+    string[] input = File.ReadAllLines("../../../input.txt");
+    List<int> first = new List<int>();
+    List<int> second = new List<int>();
+    foreach (var row in input) {
+        first.Add(int.Parse(row.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]));
+        second.Add(int.Parse(row.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]));
+    }
+    first = first.OrderBy(x => x).ToList();
+    second = second.OrderBy(x => x).ToList();
+    int sum = 0;
+    for (int i = 0; i < first.Count; i++) {
+        sum += Math.Abs(first[i] - second[i]);
+    }
+    Console.WriteLine(sum);
+}
+
+static void PartTwo() {
+    string[] input = File.ReadAllLines("../../../input.txt");
+    List<int> first = new List<int>();
+    List<int> second = new List<int>();
+    foreach (var row in input) {
+        first.Add(int.Parse(row.Split(" ", StringSplitOptions.RemoveEmptyEntries)[0]));
+        second.Add(int.Parse(row.Split(" ", StringSplitOptions.RemoveEmptyEntries)[1]));
+    }
+    first = first.Where(second.Contains).ToList();
+    second = second.Where(first.Contains).ToList();
+    int sum = 0;
+    foreach (var group in second.GroupBy(x => x)) {
+        sum += group.Key * group.Count();
+    }
+    Console.WriteLine(sum);
+}
---
+++ Day2.cs
@@ -0,0 +1,42 @@
+﻿PartOne();
+PartTwo();
+
+static void PartOne() {
+    string[] input = File.ReadAllLines("../../../input.txt");
+    int counter = 0;
+    foreach (string line in input) {
+        List<int> nums = line.Split(" ").Select(int.Parse).ToList();
+        if (IsLineSafe(nums)) { counter++; }
+    }
+    Console.WriteLine(counter);
+}
+
+static void PartTwo() {
+    string[] input = File.ReadAllLines("../../../input.txt");
+    int counter = 0;
+    foreach (string line in input) {
+        List<int> nums = line.Split(" ").Select(int.Parse).ToList();
+        if (IsLineSafe(nums)) counter++;
+        else if (ProblemDampener(nums)) counter++;
+    }
+    Console.WriteLine(counter);
+}
+
+static bool IsLineSafe(List<int> list) {
+    bool ascending = true;
+    bool descending = true;
+    for (int i = 0; i < list.Count - 1; i++) {
+        if (list[i] >= list[i + 1]) ascending = false;
+        if (list[i] <= list[i + 1]) descending = false;
+        if (Math.Abs(list[i] - list[i + 1]) < 1 || Math.Abs(list[i] - list[i + 1]) > 3) return false;
+    }
+    return ascending || descending;
+}
+
+static bool ProblemDampener(List<int> list) {
+    for (int i = 0; i < list.Count; i++) {
+        var temp = list.Where((val, index) => index != i).ToList();
+        if (IsLineSafe(temp)) return true;
+    }
+    return false;
+}
